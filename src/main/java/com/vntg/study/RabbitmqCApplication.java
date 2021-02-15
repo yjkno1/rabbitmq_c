ef82cd3194be6d95bd80ed1d9bc22b5b06876fcb
Davis <Davis@DESKTOP-2C1KEV2.mshome.net>
2021-02-15 17:05:48 +0900

    Update: project upload

---
+++ src/main/java/com/vntg/study/RabbitmqCApplication.java
@@ -0,0 +1,13 @@
+package com.vntg.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqCApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vntg/study/consumer/CustomMessageListener.java
@@ -0,0 +1,14 @@
+package com.vntg.study.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomMessageListener {
+
+    @RabbitListener(queues = "spring-boot")
+    public void receiveMessage(final CustomMessage message) {
+        System.out.println(message);
+    }
+
+}
